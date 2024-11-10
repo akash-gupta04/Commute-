183b5972f0b559a27015c5324b266a71c4e5ae6b
Akash Gupta <78719937+akash-gupta04@users.noreply.github.com>
2024-11-10 05:32:24 -0500

    Updated

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, request, session,jsonify
-import requests, googlemaps
+import requests, googlemaps,random
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Length
+from map import get_distance,get_coordinates,get_time
 
 app = Flask(__name__, template_folder="templates", static_folder="static", static_url_path='/')
 app.config['SECRET_KEY'] = 'fsaufvuyagbubikni'
@@ -26,6 +27,7 @@ class ForgotPasswordForm(FlaskForm):
     new_password = PasswordField('New Password', validators=[InputRequired(), Length(min=6)])
     confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), Length(min=6)])
 
+    
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -86,14 +88,64 @@ def forgot_password():
 
     return render_template('forgot_password.html', form=form)
 
-
+offers = {
+    "10% off Tim Hortons Gift Card": 100,
+    "Free Coffee at Starbucks": 50,
+    "5% off on Amazon Gift Card": 150,
+    "Discount on Transit Pass": 75,
+    "Buy 1 Get 1 Free Movie Ticket": 120
+}
 @app.route('/dashboard',methods=['GET','POST'])
 def dashboard():
-    carbon_perks = 100
     PrestoBalance = 5
-    results = None
-    return render_template('dashboard.html',carbon_perks=carbon_perks,results=results,PrestoBalance=PrestoBalance)
+    start = request.args.get('start')
+    end = request.args.get('end')
+    mode = request.args.get('mode')
+    get_coordinates(start)
+    get_coordinates(end)
+    bike_perks = 0
+    transit_perks = 0
+    walking_perks = 0
+    distance = get_distance(start,end,mode) 
+    if mode == 'bicycling':
+        bike_perks += distance * 15
+    elif mode == 'walking':
+        walking_perks += distance * 20
+    elif mode == 'transit':
+        transit_perks += distance * 2
+    total_perks = bike_perks + walking_perks + transit_perks
+    time = get_time(start,end,mode)
+    session['total_perks'] = total_perks
+    session['bike_perks'] = bike_perks
+    session['walking_perks'] = walking_perks
+    session['transit_perks'] = transit_perks
+    session['distance'] = distance
+    return render_template('dashboard.html',walking_perks = walking_perks, transit_perks=transit_perks,bike_perks=bike_perks, total_perks=total_perks,distance=distance,PrestoBalance=PrestoBalance,time=time)
 
+
+@app.route('/redeem',methods=['GET','POST'])
+def redeem_perks():
+    total_perks = session.get('total_perks', request.args.get('dashboard'))
+    message = ""
+
+    # Select a random offer to display
+    random_offer = random.choice(list(offers.items()))
+
+    if request.method == 'POST':
+        selected_offer = request.form.get('offer')
+        
+        # Check if the selected offer exists in the dictionary
+        if selected_offer and selected_offer in offers:
+            perk_cost = offers[selected_offer]
+            if total_perks >= perk_cost:
+                session['total_perks'] -= perk_cost
+                message = f"Successfully redeemed {selected_offer} for {perk_cost} perks!"
+            else:
+                message = "You don't have enough perks to redeem this offer."
+        else:
+            message = "Please select a valid offer."
+
+    return render_template('redeem.html', perks=session['total_perks'], offers=offers, message=message, random_offer=random_offer)
 
 
 if __name__ == '__main__':
